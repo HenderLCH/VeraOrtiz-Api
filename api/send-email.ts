599e7a9a7926f740c7060f33616e24f30316f498
HenderLCH <henderljunior@gmail.com>
2025-07-21 23:19:00 -0400

    Vercel.js changes and endpoint

--- api/send-email.ts
+++ api/send-email.ts
@@ -2,13 +2,16 @@ import type { VercelRequest, VercelResponse } from '@vercel/node';
 import { Resend } from 'resend';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
-    // Configuración manual de CORS
+  // Configuración CORS
   res.setHeader('Access-Control-Allow-Origin', 'https://vera-ortiz-o7squ9dkp-hender-labradors-projects.vercel.app');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   
+  // Manejar preflight (CORS)
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
   }
+
   if (req.method !== 'POST') {
     return res.status(405).json({ success: false, message: 'Método no permitido' });
   }
